e1d3edf45d14ad9b170334dcf81de4102770f72d
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-06-27 15:26:02 +0200

    updated sigma point transformation, added cases for 2 inputs (not tested), new cholcov for dealing with uncertainty

--- matse3/se3d_sigmas2.m
+++ matse3/se3d_sigmas2.m
@@ -9,42 +9,49 @@
 % Output:
 % xp are the 2N+1 by sizeof[group] sigmapoints, with N=6 and sizeof[group]=4,4
 % v  are the increments
-% wi are the weights as of ut_mweights
-%
-% Note: for removing the mean term (zero mean assumption) clean up the
-% weights
+% wei are the weights as of ut_mweights
 %
 % Reassemble instruction (see barefoot)
 %   OUTPUT AS SE3: use se3d_est
 %   OUTPUT AS linear: easy
 % Emanuele Ruffaldi SSSA 2015-2016
 
-function [xp,v,wei] = se3d_sigmas(d,params)
+function [xp,v,wei] = se3d_sigmas(d1,d2,params)
 
 
-n = 6; % dimensionality of the problem
+n = 12; % dimensionality of the problem
 
-if nargin == 1 || isempty(params)
+if nargin == 2 || isempty(params)
 	params = struct('alpha',0.5,'beta',2,'kappa',3-n);
 end
 
-[g,S] = se3d_get(d);
+[g1,S1] = se3d_get(d1);
+[g2,S2] = se3d_get(d2);
 
 wei = ut_mweights2(n,params.alpha,params.beta,params.kappa);
 c = wei.WC;
 
-C = cholcov2(S); % 6x6 -> 
+S = blkdiag(S1,S2);
+C = cholcov2(S); % 12x12 -> 6x6
 
 % first compute the sigma points stored ad 4x4 in this implementation
-xp = zeros(2*n+1,4,4);
-xp(1,:,:) = g; % not weighted
-v = zeros(2*n+1,6);
+xp = zeros(2*n+1,4,4,2);
+xp(1,:,:,1) = g1; % not weighted
+xp(1,:,:,2) = g2; % not weighted
+% for covariance
+v = zeros(2*n+1,12,1);
 for I=1:n
-    psi = c(I)*C(:,I);
+    psi = c(I)*C(:,I); % dimension 
+	psi1 = psi(1:6);
+    psi2 = psi(7:12);
+	
     v(I+1,:) = psi;
     v(I+1+n,:) = -psi;
-	xp(I+1,:,:) = se3_mul(se3_exp(psi),g); % weighted local motion
-	xp(I+1+n,:,:) = se3_mul(-se3_exp(psi),g); % weighred local motion
+    
+    xp(I+1,:,:,1) = se3_mul(se3_exp(psi1),g1); % weighted local motion
+	xp(I+1+n,:,:,1) = se3_mul(-se3_exp(psi1),g1); % weighred local motion
+	xp(I+1,:,:,2) = se3_mul(se3_exp(psi2),g2); % weighted local motion
+	xp(I+1+n,:,:,2) = se3_mul(-se3_exp(psi2),g2); % weighred local motion
 end
 
 % Linear form
